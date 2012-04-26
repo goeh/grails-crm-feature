f6ada9a71f1abef02b7c7b0a2bfdb2c6f509dc66
Goran Ehrsson <goran@technipelago.se>
2012-04-26 22:04:24 +0200

    Made private class ClosureToMap public

--- CrmFeatureGrailsPlugin.groovy
+++ CrmFeatureGrailsPlugin.groovy
@@ -1,4 +1,22 @@
+/*
+ * Copyright (c) 2012 Goran Ehrsson.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * under the License.
+ */
+
 import grails.plugins.crm.feature.CrmFeature
+import grails.plugins.crm.feature.ClosureToMap
 
 class CrmFeatureGrailsPlugin {
     def groupId = "grails.crm"
@@ -64,39 +82,4 @@ A feature is a unit of functionality that can be enabled or disabled per user ac
     def onShutdown = { event ->
     }
 
-}
-
-private class ClosureToMap {
-    Map props = [:]
-    String subKey
-
-    ClosureToMap(Closure c) {
-        iterate(c)
-    }
-
-    def iterate(Closure c) {
-        c.delegate = this
-        c.each {"$it"()}
-    }
-
-    def methodMissing(String name, args) {
-        if (!args.size()) return
-
-        // nested closure, recurse
-        if (args[0] in Closure) {
-            subKey = name; iterate(args[0]); subKey = null
-        }
-        else {
-            // add nested closure to properties map
-            if (subKey) {
-                Map map = props[subKey], val = [(name): args[0]]
-                props[subKey] = map ? map + val : val
-            }
-            else props[name] = args[0]
-        }
-    }
-
-    def propertyMissing(String name) {
-        name
-    }
 }
