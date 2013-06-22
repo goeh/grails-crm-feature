7e8e83f0bbbd22c7398d20ed666a4522b13744ed
Goran Ehrsson <goran@technipelago.se>
2013-06-22 15:04:13 +0200

    Added codenarc and codecoverage

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -20,16 +20,22 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://labs.technipelago.se/repo/plugin-releases-local/"
     }
     dependencies {
+        //test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:2.2.1") {
+                ":release:2.2.1") {
             export = false
         }
         runtime(":hibernate:$grailsVersion")
 
-        test(":spock:0.7") { export = false }
+        test(":spock:0.7") {
+            export = false
+            //exclude "spock-grails-support"
+        }
+        test(":codenarc:0.18.1") { export = false }
+        test(":code-coverage:1.2.6") { export = false }
 
         compile(":platform-core:1.0.RC5") { excludes 'resources' }
 
