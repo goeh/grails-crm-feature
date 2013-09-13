e93cf688033e7158f06e6c02ffb7f7a539bbe574
Goran Ehrsson <goran@technipelago.se>
2013-09-13 08:51:45 +0200

    Upgraded project to Grails 2.2.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,26 +13,28 @@ grails.project.dependency.resolution = {
         // excludes 'ehcache'
     }
     log "warn"
+    legacyResolve false
     repositories {
-        grailsHome()
         mavenRepo "http://labs.technipelago.se/repo/crm-releases-local/"
         grailsCentral()
+        mavenCentral()
         //mavenRepo "http://labs.technipelago.se/repo/plugin-releases-local/"
     }
     dependencies {
-        //test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
+        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
         build(":tomcat:$grailsVersion",
-                ":release:2.2.1") {
+                ":release:2.2.1",
+                ":rest-client-builder:1.0.3") {
             export = false
         }
         runtime(":hibernate:$grailsVersion")
 
         test(":spock:0.7") {
             export = false
-            //exclude "spock-grails-support"
+            exclude "spock-grails-support"
         }
         test(":codenarc:0.18.1") { export = false }
         test(":code-coverage:1.2.6") { export = false }
