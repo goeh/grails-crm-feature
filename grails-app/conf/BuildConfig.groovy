fffefce7642abe384a970e993ff1965d5db99a76
Goran Ehrsson <goran@technipelago.se>
2013-10-27 22:43:31 +0100

    Added dependency on cache plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,24 +1,17 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.repos.default = "crm"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
+    inherits("global") {}
     log "warn"
     legacyResolve false
     repositories {
         mavenRepo "http://labs.technipelago.se/repo/crm-releases-local/"
+        //mavenRepo "http://labs.technipelago.se/repo/plugin-releases-local/"
         grailsCentral()
         mavenCentral()
-        //mavenRepo "http://labs.technipelago.se/repo/plugin-releases-local/"
     }
     dependencies {
         test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
@@ -40,7 +33,7 @@ grails.project.dependency.resolution = {
         test(":code-coverage:1.2.6") { export = false }
 
         compile(":platform-core:1.0.RC5") { excludes 'resources' }
-
+        compile ":cache:1.1.1"
         compile "grails.crm:crm-core:latest.integration"
     }
 }
