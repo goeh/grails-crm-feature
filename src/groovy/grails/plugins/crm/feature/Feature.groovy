fec06ba50c1662a0ee76d0a65872b309f9cabea7
Goran Ehrsson <goran@technipelago.se>
2014-07-15 21:50:56 +0200

    Updated copyright

--- src/groovy/grails/plugins/crm/feature/Feature.groovy
+++ src/groovy/grails/plugins/crm/feature/Feature.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Goran Ehrsson.
+ * Copyright (c) 2014 Goran Ehrsson.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
